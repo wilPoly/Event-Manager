60cec80891ca735871018e3caf1a47c904d431c1
wilpoly <polype@pm.me>
2022-02-18 00:27:48 +0100

    Add format_phone and clean_phone

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,9 +2,31 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def format_phone(phone)
+  one, two, three = phone.match(/(\d\d\d).*(\d\d\d).*(\d\d\d\d)/).captures
+  "#{one}-#{two}-#{three}"
+end
+
+# If the phone number is less than 10 digits, assume that it is a bad number
+# If the phone number is 10 digits, assume that it is good
+# If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
+# If the phone number is 11 digits and the first number is not 1, then it is a bad number
+# If the phone number is more than 11 digits, assume that it is a bad number
+def clean_phone(phone)
+  phone = phone.scan(/\d/).join('')
+  if phone.length == 11 && phone[0] == '1'
+    format_phone(phone[1..10])
+  elsif phone.length < 10 || phone.length >= 11
+    'Bad Number'
+  else
+    format_phone(phone)
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -12,16 +34,23 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -33,19 +62,19 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  save_thank_you_letter(id, form_letter)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  phone = clean_phone(row[:homephone])
+  puts phone
 end
